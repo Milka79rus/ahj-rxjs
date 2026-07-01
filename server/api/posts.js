fe119f72f21f2f4e0467c150cd850c44b4dc97a8
Milka <mail.luda.ru@gmail.com>
2026-07-02 00:24:38 +1000

    fix: serverless functions cors configuration

--- server/api/posts.js
+++ server/api/posts.js
@@ -11,6 +11,10 @@ const posts = Array.from({ length: 5 }, () => ({
 }));
 
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
--- server/api/unread.js
+++ server/api/unread.js
@@ -1,7 +1,10 @@
 import { faker } from '@faker-js/faker';
 
 export default function handler(req, res) {
-  // Разрешаем предварительные запросы OPTIONS (CORS)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
